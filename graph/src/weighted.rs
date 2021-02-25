cbc6da79b26a25533b0fbc1af85188e130be9b15
Keith Amling <keith.amling@gmail.com>
2021-02-24 23:09:56 -0800

    Stackless find_connected

--- graph/src/weighted.rs
+++ graph/src/weighted.rs
@@ -1,6 +1,7 @@
 use ars_aa::zmodule::ZModule;
 use std::collections::HashMap;
 use std::collections::HashSet;
+use std::collections::VecDeque;
 use std::hash::Hash;
 
 // Finds generators for the space of cycle weights reachable from the given node.
@@ -26,20 +27,25 @@ pub fn find_cycle_generators<N: Hash + Clone + Eq, R: ZModule + Clone>(links: &H
 // Finds the set of connected nodes and the weight of some arbitrary path to them
 pub fn find_connected<N: Hash + Clone + Eq, R: ZModule + Clone>(links: &HashMap<N, HashSet<(N, R)>>, n: N) -> HashMap<N, R> {
     let mut acc = HashMap::new();
-    find_connected_aux(links, &mut acc, n, R::zero());
-    acc
-}
+    let mut q = VecDeque::new();
+    q.push_back((n, R::zero()));
+    loop {
+        let (n, r) = match q.pop_front() {
+            Some(e) => e,
+            None => {
+                return acc;
+            }
+        };
+        if let Some(_) = acc.get(&n) {
+            continue;
+        }
 
-fn find_connected_aux<N: Hash + Clone + Eq, R: ZModule + Clone>(links: &HashMap<N, HashSet<(N, R)>>, acc: &mut HashMap<N, R>, n: N, r: R) {
-    if let Some(_) = acc.get(&n) {
-        return;
-    }
+        acc.insert(n.clone(), r.clone());
 
-    acc.insert(n.clone(), r.clone());
-
-    for (n2, rd) in links.get(&n).unwrap() {
-        let mut r2 = r.clone();
-        r2.addmul(1, rd);
-        find_connected_aux(links, acc, n2.clone(), r2);
+        for (n2, rd) in links.get(&n).unwrap() {
+            let mut r2 = r.clone();
+            r2.addmul(1, rd);
+            q.push_back((n2.clone(), r2));
+        }
     }
 }
